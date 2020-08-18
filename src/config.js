29242df0940feda3ab89a70c088139cfe617ab58
Ashley Lee <ashleyloreal.lee@gmail.com>
2020-08-18 11:33:35 -0400

    update

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful_test_api'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://ashley2@localhost/noteful',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://ashley2@localhost/noteful_test_api'
 }
 // export default {
 //     API_ENDPOINT: `https://powerful-fjord-78205.herokuapp.com/`,
