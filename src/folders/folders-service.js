afcb77470405a869ecfc3ba53aab61066d8e217d
Ashley Lee <ashleyloreal.lee@gmail.com>
2020-08-22 14:18:44 -0400

    prepare for predeploy

--- src/folders/folders-service.js
+++ src/folders/folders-service.js
@@ -1,26 +1,26 @@
 const FoldersService = {
     getAllFolders(knex) {
-      return knex.select('*').from('notes')
+      return knex.select('*').from('folders')
     },
     insertArticle(knex, newFolder) {
       return knex
         .insert(newFolder)
-        .into('notes')
+        .into('folders')
         .returning('*')
         .then(rows => {
           return rows[0]
         })
     },
     getById(knex, id) {
-      return knex.from('notes').select('*').where('id', id).first()
+      return knex.from('folders').select('*').where('id', id).first()
     },
     deleteFolder(knex, id) {
-      return knex('notes')
+      return knex('folders')
         .where({ id })
         .delete()
     },
     updateFolder(knex, id, newFolderFields) {
-      return knex('notes')
+      return knex('folders')
         .where({ id })
         .update(newFolderFields)
     },
