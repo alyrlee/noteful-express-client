0c23821c57ebd11d0f0cc95d437d63157e855dcf
Ashley Lee <ashleyloreal.lee@gmail.com>
2020-09-04 12:04:14 -0400

    prepare for deploy

--- src/config.js
+++ src/config.js
@@ -4,9 +4,3 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://ashley2@localhost/noteful',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://ashley2@localhost/noteful_test_api'
 }
-// export default {
-//     API_ENDPOINT: `https://fierce-taiga-41664.herokuapp.com//`,
-//     API_KEY: process.env.REACT_APP_API_KEY,
-//   }
-//⬢ fierce-taiga-41664
-//https://fierce-taiga-41664.herokuapp.com/ | https://git.heroku.com/fierce-taiga-41664.git
